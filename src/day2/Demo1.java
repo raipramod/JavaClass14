30d73d7944ddc0c06b1d734720d34aaefd45da90
raipramod <pramodchamlingrai@gmail.com>
2021-07-23 22:00:01 -0600

    added java practice code

---
+++ src/day2/Demo1.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+//		243 
+		byte b = 123;
+		short s = 3222;
+		int i = 33245;
+		//long l = 222.33l;
+		float f = 34.22f;
+		double d = 45.89;
+		char c = '1';
+		char c1 = '&';
+//		Unicode char
+		
+		char u = '\u0001';
+		
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println("Unicode Char is " +u);
+	
+
+	}
+
+}
